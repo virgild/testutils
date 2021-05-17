22431101a2ba0193df6a53811b3e9fed58a06285
Virgil Dimaguila <virgild@gmail.com>
2021-05-17 14:12:23 -0400

    Replace names generator with just random strings.

--- mysqlbox/random.go
+++ mysqlbox/random.go
@@ -3,14 +3,24 @@ package mysqlbox
 import (
 	"math/rand"
 	"time"
-
-	"github.com/docker/docker/pkg/namesgenerator"
 )
 
 func init() {
 	rand.Seed(time.Now().UnixNano())
 }
 
+var charset = []rune("abcdefghijklmnopqrstuvwxyz0123456789")
+var charsetLen = len(charset)
+
 func randomID() string {
-	return namesgenerator.GetRandomName(1)
+	return randStr(5)
+}
+
+func randStr(length int) string {
+	c := make([]rune, length)
+	for n := range c {
+		c[n] = charset[rand.Intn(charsetLen)]
+	}
+
+	return string(c)
 }
